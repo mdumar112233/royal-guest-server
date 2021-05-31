41507ca967980cd396c26b2103d4d3d55aafeae6
md umar faruk <mdumar11233>
2021-05-31 21:56:03 +0600

    start server site

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ij0ac.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello new node start!')
+})
+
+client.connect(err => {
+  const hotelCollection = client.db("royal-guest").collection("homeInfo");
+
+  //   app.post('/product', (req, res) => {
+//       const product = req.body;
+//       collection.insertOne(product);
+//       then(result => {
+//           res.send(result.insertedCount > 0)
+//       })
+//   })
+  app.get('/hotelInfo', (req, res) => {
+    hotelCollection.find({})
+      .toArray((err, documents) => {
+          res.send(documents)
+      })
+  })
+
+  app.get('/singleHotel/:id', (req, res) => {
+    hotelCollection.find({id: req.params.id})
+      .toArray((err, documents) => {
+          res.send(documents)
+      })
+  })
+
+});
+
+
+
+app.listen(port)
