dd269428fc28bb7db8b56b026f217887925bdb7c
md umar faruk <mdumar11233>
2021-06-01 17:17:49 +0600

    added new code

--- index.js
+++ index.js
@@ -7,19 +7,22 @@ require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ij0ac.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 
-app.get('/', (req, res) => {
-  res.send('Hello new node start here!')
-})
+
 
 client.connect(err => {
   const hotelCollection = client.db("royal-guest").collection("homeInfo");
 
+  app.get('/', (req, res) => {
+    res.send('Hello new node start here!')
+  })
+  
   app.get('/hotelInfo', (req, res) => {
     hotelCollection.find({})
       .toArray((err, documents) => {
